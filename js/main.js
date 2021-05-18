fab36bf97663143a35ace7df53a1c919b7d16d29
Rikesh Shrestha <rickxtha@gmail.com>
2021-05-18 12:06:15 +0545

    lots of bug searchig for solution

---
+++ js/main.js
@@ -0,0 +1,23 @@
+
+// // var preloader=document.getElementById('overlayer');
+// // function myFunction(){
+// //   preloader.style.display='none';
+// // }
+
+// setTimeout(function(){
+//   $('#overlayer').fadeToggle();
+//   },7500);
+
+setTimeout(function(){
+  $('.elegant').fadeToggle();
+  },3000);
+
+ 
+
+    setTimeout(function(){
+      $('.sexy').fadeToggle();
+      },5000);
+
+      setTimeout(function(){
+        $('.you').fadeToggle();
+        },7500);
